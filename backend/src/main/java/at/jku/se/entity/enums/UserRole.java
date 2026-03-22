6d70db8f39a4bf4f103f1db542ca69634153506b
arminhamzic01 <arminhamzic01@gmail.com>
2026-03-22 17:38:27 +0100

    #16 [2h] adding folder structure inkl. User Management

---
+++ backend/src/main/java/at/jku/se/entity/enums/UserRole.java
@@ -0,0 +1,10 @@
+package at.jku.se.entity.enums;
+
+/**
+ * Defines the access roles a user can have in the system.
+ * OWNER has full access; MEMBER can only control devices.
+ */
+public enum UserRole {
+    OWNER,
+    MEMBER
+}
